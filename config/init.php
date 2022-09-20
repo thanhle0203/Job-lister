88d3f5a17bfeee3afd501b3ffde3733c208bab79
Thanh Le <thanhlevan0203@gmail.com>
2022-09-20 03:32:59 -0700

    Created function autoload

--- config/init.php
+++ config/init.php
@@ -3,7 +3,21 @@
 require_once 'config.php';
 
 // Autoloader
-spl_autoload_register(function($class_name) {
+function spl__autoload($class_name){
+    //$class_name = strtolower($class_name);
+    //$path = "{$class_name}.php";
+   // $iterator = new DirectoryIterator(dirname(__FILE__));
+    //$files = $iterator->getPath()."/lib/".$class_name.".php";
+    //if (file_exists($path)){
+       //require_once($path);
+    //} else {
+      // die("The file {$class_name}.php could not be found!");
+    //} 
+    //require_once($path);
+    require_once 'lib/'.$class_name. '.php';
+}
+
+//spl_autoload_register(function($class_name) {
     //$class_name = strtolower($class_name);
     //$path = "{$class_name}.php";
    // if (file_exists($path)){
@@ -12,16 +26,22 @@ spl_autoload_register(function($class_name) {
        // die("The file {$class_name}.php could not be found!");
     //}
     //require_once 'lib/'.$class_name. '.php';
-    $iterator = new DirectoryIterator(dirname(__FILE__));
-    $files = $iterator->getPath()."/lib/".$class_name.".class.php";
+   // $iterator = new DirectoryIterator(dirname(__FILE__));
+   // $files = $iterator->getPath()."/lib/".$class_name.".php";
     
-    if (file_exists($files)) {
-        include($files);
-    } else {
-       die("Warning:The file {$files}.class.php could not be found!");
+    //if (file_exists($files)) {
+        //include($files);
+   // } else {
+      // die("Warning:The file {$files}.class.php could not be found!");
     
-    }
-});
+    //}
+//});
+
+//spl_autoload_register(function ($class) {
+    //@require_once('lib/type/' . $class . '.php');   
+   // @require_once('lib/size/' . $class . '.php');
+//});
 
 ?>
-echo 'test';
+
+
